5ee4c7f731bc06adcde720a635ff121d64a3cd8c
Mickael Haye <mickael.haye@gmail.com>
2023-09-22 15:48:16 +0200

    passage du nombre de user pour 2 ème test à 100000

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
@@ -86,7 +86,7 @@ public class TestPerformance {
 
 		// Users should be incremented up to 100,000, and test finishes within 20
 		// minutes
-		InternalTestHelper.setInternalUserNumber(10000);
+		InternalTestHelper.setInternalUserNumber(100000);
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
