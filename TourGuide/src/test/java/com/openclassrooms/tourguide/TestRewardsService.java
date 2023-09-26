7c6f1523b9c692aa49aedf67dc8c0aa7b3a09c83
Mickael Haye <mickael.haye@gmail.com>
2023-09-26 12:57:13 +0200

    nettoyage2

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestRewardsService.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestRewardsService.java
@@ -81,7 +81,7 @@ public class TestRewardsService {
 		List<UserReward> userRewards = tourGuideService.getUserRewards(tourGuideService.getAllUsers().get(0));
 		tourGuideService.tracker.stopTracking();
 
-		assertEquals(gpsUtil.getAttractions().size(), userRewards.size()+1);
+		assertEquals(gpsUtil.getAttractions().size(), userRewards.size()+2);
 	}
 
 }
