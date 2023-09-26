a8bd88913c4c7fa5fa5a718a79e7b24f0b6e9186
Mickael Haye <mickael.haye@gmail.com>
2023-09-26 11:18:14 +0200

    nettoyage

--- TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
@@ -32,7 +32,7 @@ public class RewardsService {
 	private final RewardCentral rewardsCentral;
 	private final List<Attraction> attractions;
 
-	private final ExecutorService executorService = Executors.newFixedThreadPool(100);
+	private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 	
 	public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
 		this.gpsUtil = gpsUtil;
